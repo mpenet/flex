8ad2d0d7cf621a2c16a3c1c7923fa8942554752c
Max Penet <max.penet@exoscale.ch>
2021-03-11 20:05:44 +0100

    add license

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject cc.qbits/flex "1.0.0-alpha1-SNAPSHOT"
   :url "https://github.com/mpenet/flex"
 
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.10.2"]
                  [exoscale/ex "0.3.17"]
                  [exoscale/interceptor "0.1.9"]]
